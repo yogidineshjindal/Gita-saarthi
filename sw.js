b160104ac022079ae8cfb986eae1d23ebbe46820
yogiDineshJindal <ishikagarg2609@gmail.com>
2026-09-24 10:07:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = "krishna-vani-v1";
+self.addEventListener("install", (e) => { self.skipWaiting(); });
+self.addEventListener("activate", (e) => { self.clients.claim(); });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
